b3cbcc0299bedde0989c0036cd2041383a0fcc7f
gstawde <gargi.tawde01@gmail.com>
2023-05-07 13:54:31 -0700

    Backlog Page functionality

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright Gargi Tawde (c) 2023.
+ */
+
+import KanbanAPI from "./api/KanbanAPI.js";
+import Kanban from "./view/Kanban.js";
+
+new Kanban(
+    document.querySelector(".kanban")
+);
+
---
+++ JS/view/Kanban.js
@@ -0,0 +1,37 @@
+/*
+ * Copyright Gargi Tawde (c) 2023.
+ */
+import Column from "./Column.js";
+
+export default class Kanban {
+    constructor(root) {
+        this.root = root;
+
+        Kanban.columns().forEach(column => {
+            const columnView = new Column(column.id, column.title);
+
+            this.root.appendChild(columnView.elements.root);
+        });
+    }
+
+    static columns() {
+        return [
+            {
+                id: 0,
+                title: "Backlog"
+            },
+            {
+                id: 1,
+                title: "Not Started"
+            },
+            {
+                id: 2,
+                title: "In Progress"
+            },
+            {
+                id: 3,
+                title: "Completed"
+            },
+        ];
+    }
+}
